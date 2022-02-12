fb7b6adf38ade43e21ff356d63ea8e806259acf0
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-02-12 22:36:55 +0300

    1202 2236

--- Week_3/geometric_calculator_GUI.py
+++ Week_3/geometric_calculator_GUI.py
@@ -1,7 +1,11 @@
 
 
 class Shape():
+    title = 'Shape'
     # для плоских фигур мера Лебега - это площадь, а для объемных - объем
+    def __init__(self, a):
+        self.a = a
+
     def lebesgue_measure(self):
         print('Метод "Shape"')
         # pass
@@ -12,23 +16,91 @@ class Shape():
 
 
 class PlanShape(Shape):
+    title = 'PlanShape'
+    def __init__(self):
+        self.area = self.lebesgue_measure()
+        self.perimeter = self.border_lebesgue_measure()
+
     def lebesgue_measure(self): # площадь фигуры на плоскости
+        self.area = -1
         print('Метод "PlanShape"')
 
     def border_lebesgue_measure(self): #  периметр плоской на плоскости
+        self.perimeter = -1
         print('Метод "PlanShape"')
 
 class Parallelogram(PlanShape):
-    pass
+    title = 'Parallelogram'
+    def __init__(self, a, b, alpha):
+        super().__init__()
+        self.b = b
+        self.alpha = alpha
+        self.betta = 180 - self.alpha
+    def info(self):
+        print(f'TITLE: {self.title}')
+        print(f'\tself.a = {self.a}')
+        print(f'\tself.b = {self.b}')
+        print(f'\tself.alpha = {self.alpha}')
+        print(f'\tself.betta = {self.betta}')
+
+
+class Square(Parallelogram):
+    title = 'Square'
+    def __init__(self, a):
+        self.a = a
+        self.b = a
+        self.alpha = 90
+        self.betta = 180 - self.alpha
+        print(f'self.a = {self.a}')
+
+    def lebesgue_measure(self):
+        return self.a*self.a
+
+    def border_lebesgue_measure(self):
+        return 4*self.a
+
+class Rectangle(Parallelogram):
+    title = 'Rectangle'
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        self.alpha = 90
+        self.betta = 180 - self.alpha
+
+    def lebesgue_measure(self):
+        return self.a*self.b
+
+    def border_lebesgue_measure(self):
+        return 2*(self.a + self.b)
+
 
 class StereoShape(Shape):
+    title = 'StereoShape'
     def lebesgue_measure(self): # объем фигуры в пространстве
+        self.volume = -1
         pass
 
     def border_lebesgue_measure(self): #  площадь поверхности фигуры в пространстве
+        self.border_area = -1
         pass
 
 
-figure = Parallelogram()
 
-figure.lebesgue_measure()
+figure = Square(10)
+print(f'figure = Square(10)')
+print(figure.lebesgue_measure())
+print(figure.border_lebesgue_measure())
+figure.info()
+
+figure = Rectangle(10, 15)
+print(f'figure = Rectangle(10)')
+print(figure.lebesgue_measure())
+print(figure.border_lebesgue_measure())
+figure.info()
+
+figure = PlanShape()
+print(f'figure = PlanShape()')
+print(figure.lebesgue_measure())
+print(figure.border_lebesgue_measure())
+print(f'figure.area() = {figure.area}')
+print(f'figure.perimeter() = {figure.perimeter}')
