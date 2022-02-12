787d9838852df2c7b8241aeab7b95b096c826abb
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-02-12 11:55:19 +0300

    1202 1155

--- Week_3/sandbox.py
+++ Week_3/sandbox.py
@@ -1,11 +1,11 @@
 from tkinter import *
 from tkinter.ttk import Combobox, Notebook
 
-def clicked():
+def clicked(i):
     # lbl.configure(text="Кнопка нажата", fg="red")
-    res = f"Считано {input_txt.get()} и {combo.get()}"
-    lbl.configure(text=res, fg="green")
-    input_txt.configure(width=5, state='disabled')
+    res = f"Считано {input_txts[i].get()} и {combos[i].get()}"
+    lbls[i].configure(text=res, fg="green")
+    input_txts[i].configure(width=5, state='disabled')
 
 window = Tk()
 window.title("Добро пожаловать в приложение!")
@@ -14,11 +14,33 @@ tab_control = Notebook(window)
 
 tab_names = tuple(['Первая', 'Вторая', 'Третья', 'Четвёртая'])
 tabs = []
+lbls = []
+lbls2 = []
+btns = []
+input_txts = []
+combos = []
 
 for i, tab_name in enumerate(tab_names):
     tab = Frame(tab_control)
     tabs.append(tab)
     tab_control.add(tabs[i], text=tab_name)
+    lbl = Label(tabs[i], text=f'Надпись {i}', font=("Arial Bold", 14))
+    lbl.grid(column=0, row=0)
+    lbls.append(lbl)
+    lbl2 = Label(tabs[i], text=f'Надпись_2 {i}', font=("Arial Bold", 14))
+    lbl2.grid(column=1, row=0)
+    lbls2.append(lbl2)
+    btn = Button(tabs[i], text="Кнопка", command=clicked(i))
+    btn.grid(column=2, row=0)
+    btns.append((btn))
+    input_txt = Entry(tabs[i], width=10)
+    input_txt.grid(column=2, row=2)
+    input_txts.append(input_txt)
+    combo = Combobox(tabs[i])
+    combo['values'] = (1, 2, 3, 4, 5)
+    combo.current(0)
+    combo.grid(column=2, row=3)
+    combos.append(combo)
 
 tab_control.pack(expand=1, fill='both')
 
@@ -28,21 +50,21 @@ W = 250
 window.geometry(f'{H}x{W}')
 
 # добавление надписей
-x = 0
-y = 0
-lbl = Label(window, text=f'({x}, {y})', font=("Arial Bold", 14))
-lbl.grid(column=x, row=y)
-
-x = 125
-y = 25
-lbl2 = Label(window, text=f'({x}, {y})', padx=x, pady=y, font=("Arial Bold", 14))
-lbl2.grid(column=x, row=y)
-
-# добавление кнопок
-x = 1
-y = 0
-btn = Button(tabs[0], text="Кнопка", command=clicked)
-btn.grid(column=x, row=y)
+# x = 0
+# y = 0
+# lbl = Label(tabs[0], text=f'({x}, {y})', font=("Arial Bold", 14))
+# lbl.grid(column=x, row=y)
+#
+# x = 125
+# y = 25
+# lbl2 = Label(tabs[0], text=f'({x}, {y})', padx=x, pady=y, font=("Arial Bold", 14))
+# lbl2.grid(column=x, row=y)
+#
+# # добавление кнопок
+# x = 1
+# y = 0
+# btn = Button(tabs[0], text="Кнопка", command=clicked)
+# btn.grid(column=x, row=y)
 
 # # создание поля ввода
 # input_txt = Entry(window, width=10)
