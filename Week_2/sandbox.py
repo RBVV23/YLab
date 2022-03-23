7a2273a06ceb13124cde8527b2fb51e7bae3df56
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-03-23 19:14:25 +0300

    2303 1914

--- Week_2/sandbox.py
+++ Week_2/sandbox.py
@@ -1,14 +1,14 @@
 
 red = '\033[31m'
-blue ='\033[34m'
+blue = '\033[34m'
 
-marks = ['\033[31m X\033[00m','\033[34m O\033[00m']
+marks = ['\033[31m X\033[00m', '\033[34m O\033[00m']
 
 CELL_WIDTH = 10
 
-nim=50
-nim='srt'
-nim='\033[31mX'
+nim = 50
+nim = 'srt'
+nim = '\033[31mX'
 
 print(f'{marks[0]:^{CELL_WIDTH}}', end='|')
 
